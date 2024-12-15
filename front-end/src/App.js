8f51151b8269ac3b62ccf31decd9b951149f73eb
rafayhanan <mrafayhanan@gmail.com>
2024-12-15 13:51:15 +0500

    create react app

---
+++ front-end/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-500">
+      <h1 className="text-white text-4xl font-bold">Hello, Tailwind CSS!</h1>
+    </div>
+  );
+}
+
+export default App;
